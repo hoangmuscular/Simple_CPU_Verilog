5f8ae95eca0e2bfda330261be55244d34a10308b
hoangmuscular <hoang.lebeo246@hcmut.edu.vn>
2026-05-27 22:58:30 +0700

    Implement sequential logic for program counter

--- src/PC.v
+++ src/PC.v
@@ -6,8 +6,12 @@ module PC(
     input [31:0] data_in,
     output reg [31:0] pc_out
     );
-    // 1. TODO: Implement a sequential logic block triggered by the positive edge of the clock
-    // 2. TODO: Implement synchronous active-high reset to clear the program counter
-    // 3. TODO: Implement logic to load a new address into the program counter when ld_pc is active
-    // 4. TODO: Implement logic to increment the program counter when inc_pc is active
+    always @( posedge clk) begin
+   if (rst)
+      pc_out <= 4'd0;
+   else if (ld_pc)
+      pc_out <= data_in;
+   else if (inc_pc)
+      pc_out <= pc_out + 1;      
+end
 endmodule
