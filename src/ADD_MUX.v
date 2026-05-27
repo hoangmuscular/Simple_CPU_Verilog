33f7f8bed809e76b483f22ec77b90f125d97ef8b
hoangmuscular <hoang.lebeo246@hcmut.edu.vn>
2026-05-27 23:06:16 +0700

    Implement address selection logic in ADD_MUX

--- src/ADD_MUX.v
+++ src/ADD_MUX.v
@@ -6,6 +6,5 @@ module address_mux #(
     input sel, 
     output [WIDTH-1:0] addr_out 
 );
-    // 1. TODO: Implement combinational logic to select between pc_addr and ir_addr based on the sel signal
-    // 2. TODO: Output pc_addr when sel is 1, and ir_addr when sel is 0
+    assign add_out = (sel) ? pc_add : ir_add;
 endmodule
